ee77062a3f53a7e2f5cd37a98450d93aef7d0c70
prembala1710 <83074315+prembala1710@users.noreply.github.com>
2021-06-07 09:20:49 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import "/public/styles.css";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Note from "./Note.jsx";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+      <Note />
+      <Note />
+    </div>
+  );
+}
+export default App;
